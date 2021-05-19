e7b68fb72aa8c8f4db27343ca6c3235d30eb076a
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-19 14:22:40 +0200

    Bar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 /*
-Predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure cliccando su un pulsante, il testo digitato viene aggiunto alla lista (all'interno di un nuovo oggetto con proprietà "completed" settata a false).
-*/
-/*
 BONUS:
 Al click sull'intero elemento della lista, si modifica il valore della proprietà completed da false a true, e viceversa in caso di successivi click (toggle).
 Quando un elemento ha la proprietà "completed" settata a true, il testo corrispondente sarà mostrato barrato (esisterà una proprietà CSS per ottenere questo effetto???).
@@ -48,6 +45,15 @@ const app = new Vue(
                 } else {
                     this.newToDo = '';
                     this.errorText = "Errore, inserisci le cose da fare!";
+                }
+            },
+            addRemoveBar: function() {
+                if (!this.toDoList.completed) {
+                    this.toDoList.completed = true;
+                    console.log("true");
+                } else {
+                    this.toDoList.completed = false;
+                    console.log("false");
                 }
             }
         }
