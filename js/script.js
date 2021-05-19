b49446716a609be21da8e053ebd30739c711a219
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-19 13:06:52 +0200

    toDoList Array

--- js/script.js
+++ js/script.js
@@ -5,8 +5,42 @@ Creare con Vue una to-do list, in cui ogni elemento è un oggetto composto da:
 Stampare in pagina un item per ogni elemento della to-do list.
 Ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista.
 Predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure cliccando su un pulsante, il testo digitato viene aggiunto alla lista (all'interno di un nuovo oggetto con proprietà "completed" settata a false).
-
+*/
+/*
 BONUS:
 Al click sull'intero elemento della lista, si modifica il valore della proprietà completed da false a true, e viceversa in caso di successivi click (toggle).
 Quando un elemento ha la proprietà "completed" settata a true, il testo corrispondente sarà mostrato barrato (esisterà una proprietà CSS per ottenere questo effetto???).
 */
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            toDoList: [
+                {
+                    text: "A",
+                    completed: false,
+                },
+                {
+                    text: "B",
+                    completed: false,
+                },
+                {
+                    text: "C",
+                    completed: false,
+                },
+                {
+                    text: "D",
+                    completed: false,
+                },
+                {
+                    text: "E",
+                    completed: false,
+                }
+            ]
+        },
+        methods: {
+
+        }
+    }
+);
