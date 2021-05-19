e6b9dfa4ccdd72aa1c227e274280969711867812
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-19 13:13:45 +0200

    Delete Function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 /*
-Creare con Vue una to-do list, in cui ogni elemento è un oggetto composto da:
-- testo
-- "completed", un booleano settato inizialmente a false.
-Stampare in pagina un item per ogni elemento della to-do list.
-Ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista.
 Predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure cliccando su un pulsante, il testo digitato viene aggiunto alla lista (all'interno di un nuovo oggetto con proprietà "completed" settata a false).
 */
 /*
@@ -40,7 +35,9 @@ const app = new Vue(
             ]
         },
         methods: {
-
+            deleteToDo: function(deleteIndex) {
+                this.toDoList.splice(deleteIndex, 1);
+            },
         }
     }
 );
