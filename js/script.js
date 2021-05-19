a6867f291e42884fef9e4159042e71f01d3d7b73
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-19 15:07:09 +0200

    // Refactoring

--- js/script.js
+++ js/script.js
@@ -29,25 +29,37 @@ const app = new Vue(
         },
         methods: {
             deleteToDo: function(deleteIndex) {
+                // Nell'array "toDoList" rimuovo l'oggetto con indice "deleteIndex"
                 this.toDoList.splice(deleteIndex, 1);
             },
             addToDo: function() {
+                // Il nuovo testo "newToDo" non deve essere vuoto e non deve essere un numero
                 if (this.newToDo.trim().length > 0 && isNaN(this.newToDo)) {
+                    // Generare un nuovo testo "newToDo" minuscolo capitalizzato
+                    this.newToDo = this.newToDo[0].toUpperCase() + this.newToDo.substring(1);
+                    // Inserire nell'array "toDoList" il nuovo oggetto avente text "newToDo" e completed "false"
                     this.toDoList.push({text: this.newToDo, completed: false})
+                    // Pulire l'input text
                     this.newToDo = '';
+                    // Rimuovere il messaggio di errore
                     this.errorText = '';
                 } else {
+                    // Pulire l'input text
                     this.newToDo = '';
+                    // Inserire il messaggio di errore
                     this.errorText = "Errore, inserisci le cose da fare!";
                 }
             },
             addRemoveBar: function(objectIndex) {
+                // Assegnare ad una varabile il valore del completed
                 let value = this.toDoList[objectIndex].completed;
+                // Se il valore è "false" diventerà "true" e viceversa
                 if (!value) {
                     value = true;
                 } else {
                     value = false;
                 }
+                // Restituisco al completed il valore della variabile
                 this.toDoList[objectIndex].completed = value;
             }
         }
